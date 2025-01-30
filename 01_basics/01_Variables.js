254b9ac49897ef2cc6e147f9b0f3c8084ba77290
Ritesh Yadav <142344354+RiteshYadav11@users.noreply.github.com>
2025-01-30 19:44:54 +0000

    datatypes, conversionOperations add

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,14 @@
+const counter = 1
+let studentName = "Ritesh"
+var studentPlace = "Shahad"
+studentemail = "Ritesh@gmail.com"
+
+let studentpass;
+console.log(counter);
+
+/*
+Prefer do not use var
+because of issue in block and functional scope
+*/
+
+console.table([counter,studentName,studentPlace,studentemail,studentpass])
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueodscore = Number(score)
+
+console.log(typeof valueodscore)
+console.log(valueodscore)
+
+
+/* 
+"33" --> 33
+if score = "32abc" --> NaN (Not a number) after conversion to number
+if score True --> 1 and False --> 0
+"undefined" --> NaN
+
+// 1 => true; 0 => false
+//=> false
+// "hitesh" => true
+
+*/
