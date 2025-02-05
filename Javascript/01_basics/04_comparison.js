4612218ddcf9f899506af5796b053e1b157a771e
RiteshYadav11 <riteshyadav80542@gmail.com>
2025-02-05 21:15:07 +0530

    push datatypes

---
+++ Javascript/01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log("2" > 1);
+
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// null > 0 → null converts to 0, so 0 > 0 → false.
+// null == 0 → null only equals undefined, so false.
+// null >= 0 → null converts to 0, so 0 >= 0 → true.
+// Comparison (>=, >, <, <=) converts null to a number, but == does not.
+
+
+// Narayana School
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+console.log("02" === 2);
---
+++ Javascript/01_basics/datatypes_summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Symbol, Null , undefined, Bigint
+
+// Non Primitive : 
+// Array , Object , Function
+
+let name = "Ritesh" ;
+
+let rollnumber = 123 ;
+
+let eventid = 123.4 ;
+
+let isLogged = true ;
+
+let age = null ;
+
+let sessionid ;
+
+const biggernumber = 2222222222222222222222n;
+
+
+let username = ["Ritesh" , "Aman" , "Rahul"]
+
+let dict = {
+    "name" : "Ritesh",
+    "age" : 20,
+    "egilible" : true
+}
+
+console.log(typeof age);
+console.log(typeof sessionid);
+console.log(typeof biggernumber,"Hii");
+
+console.log(username);
+
+const myfunc = function(){
+    console.log("Hello Ritesh")
+}
+
+myfunc()
